7a0c778107aa6cc85bc65779f81b8a8a96a7a572
upidea <upidea@users.noreply.github.com>
2026-02-21 08:22:44 +0000

    deploy: 20cc7c28cfb359ce0865a32dfb6a57e2ec25182e

--- assets/ok_1.md.Bcj98Rp-.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as a}from"./chunks/framework.DaHRzlxw.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"ok/1.md","filePath":"ok/1.md"}'),o={name:"ok/1.md"};function r(s,c,n,_,p,d){return t(),a("div")}const f=e(o,[["render",r]]);export{m as __pageData,f as default};
