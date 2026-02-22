0641127691097b26da76bf061203be78ad568375
upidea <upidea@users.noreply.github.com>
2026-02-22 10:36:38 +0000

    deploy: 2752ea14ea8a2c73c71ae3d7aa69ef89fc431a23

---
+++ assets/development_development.md.CqAkQVFA.js
@@ -0,0 +1,11 @@
+import{_ as n,C as a,o as r,c as d,af as t,E as l,w as i,j as m}from"./chunks/framework.DaHRzlxw.js";const x=JSON.parse('{"title":"开发笔记","description":"","frontmatter":{"date":"2026-02-21T12:19:00.000Z"},"headers":[],"relativePath":"development/development.md","filePath":"development/development.md"}'),p={name:"development/development.md"};function c(h,e,_,k,u,b){const o=a("markmap"),s=a("ClientOnly");return r(),d("div",null,[e[1]||(e[1]=t('<h1 id="开发笔记" tabindex="-1">开发笔记 <a class="header-anchor" href="#开发笔记" aria-label="Permalink to &quot;开发笔记&quot;">​</a></h1><p><a href="/development/tmxlite.html">tmxlite</a></p><h2 id="roadmap" tabindex="-1">RoadMap <a class="header-anchor" href="#roadmap" aria-label="Permalink to &quot;RoadMap&quot;">​</a></h2>',3)),l(s,null,{default:i(()=>[l(o,{width:"100%",height:"500px",id:"markmap-1771756593065-4kuzniadq"},{default:i(()=>[...e[0]||(e[0]=[m("pre",null,`# 游戏框架
+## 主循环
+### 资源加载1
+### 资源加载2
+### 资源加载3
+### 资源加载4
+## 事件处理
+## 渲染
+### 资源加载66
+### 资源加载77
+`,-1)])]),_:1})]),_:1}),e[2]||(e[2]=t('<h2 id="todo" tabindex="-1">TODO <a class="header-anchor" href="#todo" aria-label="Permalink to &quot;TODO&quot;">​</a></h2><p>想做的事情清单呗</p><ul class="task-list-container"><li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-0" disabled="disabled"><label class="task-list-item-label" for="task-item-0"> 已经完成的内容</label></li><li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-1" checked="checked" disabled="disabled"><label class="task-list-item-label" for="task-item-1"> 引入 vitepress 做文档管理</label></li></ul><h2 id="资料" tabindex="-1">资料 <a class="header-anchor" href="#资料" aria-label="Permalink to &quot;资料&quot;">​</a></h2><ul><li><a href="https://onecompiler.com/cpp" target="_blank" rel="noreferrer">在线编译器</a></li><li><a href="/development/markdown-examples.html">markdown-examples</a></li><li><a href="/development/api-examples.html">api-examples</a></li></ul>',5))])}const T=n(p,[["render",c]]);export{x as __pageData,T as default};
